b3c865ddac32961eb2edf283f5c817e20bf51b89
Krushnal09 <104764240+Krushnal09@users.noreply.github.com>
2022-07-17 21:10:03 +0530

    1

---
+++ krushnal programs/programs/js/respo.js
@@ -0,0 +1,14 @@
+/*burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+nav-List = document.querySelector('.navlist')
+rightNav = document.querySelector('.rightnav')
+
+burger.addEventListener('click', ()=>
+{
+    rightNav.classList.toggle('v-class')
+    nav-List.Class.toggle('v-class');
+    navbar.Class.toggle('h-nav');
+
+}) 
+*/
+
